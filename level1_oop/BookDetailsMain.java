f87fee727d169d827f72e33271b71bbf18bda2a2
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-01-16 16:00:59 +0530

    added BookDetailsMain.java

---
+++ level1_oop/BookDetailsMain.java
@@ -0,0 +1,30 @@
+class Book {
+    // Fields (Attributes)
+    private String title;
+    private String author;
+    private double price;
+
+    // Constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: INR " + price+"/-");
+    }
+}
+
+public class BookDetailsMain {
+    public static void main(String[] args) {
+        // Create Book object
+        Book book = new Book("Harry Potter Series", "J.K. Rowling", 999.99);
+
+        // Display details of the book
+        book.displayDetails();
+    }
+}
